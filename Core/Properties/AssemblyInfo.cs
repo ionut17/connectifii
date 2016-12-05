c80e6437717b94b488b43c8a09a654572e48a81c
ionut.iacob <ionut.iacob17@gmail.com>
2016-12-05 08:38:28 +0200

    Added main structure to project

---
+++ Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("fb6f8d7e-0873-4cfb-9b6d-26afcb684d34")]
---
+++ README.md
@@ -0,0 +1,17 @@
+# Faculty Database API
+API de conectare la baza de date a FII
+
+# Board SCRUM
+Board: https://trello.com/b/bSoKHbkR
+
+# Cerinte generale
+- Sistem de prezenta
+- Sistem de notare
+- Adaptabil la mai multe materii simultan
+
+# Echipa
+- Adascalitei Anca Luiza
+- Gadioi Alexandra
+- Giosanu Eveline Andreea
+- Gordin Stefan
+- Iacob Robert Ionut
