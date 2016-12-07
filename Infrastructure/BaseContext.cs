70e322cae69987b58a7eedaa0fc739810d9a877d
ionut.iacob <ionut.iacob17@gmail.com>
2016-12-07 14:34:12 +0200

    Fixed missing depressing dependencies, updated connection string, created database

--- Infrastructure/BaseContext.cs
+++ Infrastructure/BaseContext.cs
@@ -6,5 +6,11 @@ namespace Infrastructure
     public class BaseContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EntityModel;Trusted_Connection=True;");
+        }
     }
 }
---
+++ Infrastructure/Migrations/BaseContextModelSnapshot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Infrastructure;
+
+namespace Infrastructure.Migrations
+{
+    [DbContext(typeof(BaseContext))]
+    partial class BaseContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-preview1-22509")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Core.Student", b =>
+                {
+                    b.Property<string>("RegistrationNumber")
+                        .ValueGeneratedOnAdd()
+                        .HasMaxLength(16);
+
+                    b.Property<DateTime>("BirthDate");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasMaxLength(20);
+
+                    b.Property<string>("Group")
+                        .IsRequired()
+                        .HasMaxLength(2);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(20);
+
+                    b.Property<int>("Year")
+                        .HasMaxLength(1);
+
+                    b.HasKey("RegistrationNumber");
+
+                    b.ToTable("Students");
+                });
+        }
+    }
+}
