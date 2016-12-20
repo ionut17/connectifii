9c840b9ea5e16804e0d4ec585a9902af5814c0d1
eveline.giosanu <eveline.giosanu@gmail.com>
2016-12-20 19:32:54 +0200

    Added POST for teachers => TeachersController

---
+++ Core/TeacherDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core
+{
+    public class TeacherDto
+    {
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<TeacherCourse> TeacherCourse { get; set; }
+    }
+}
