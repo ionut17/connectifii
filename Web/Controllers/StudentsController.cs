72a2970405cd07694d19ad2199f27110b251d9d5
ionut.iacob <ionut.iacob17@gmail.com>
2016-12-21 11:35:37 +0200

    Working on integrating POST route for Students. Added StudentDTO. Pending solving Group conflicts.

--- Web/Controllers/StudentsController.cs
+++ Web/Controllers/StudentsController.cs
@@ -1,13 +1,85 @@
-﻿using Core;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
 using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
+    [Route("api/students")]
     public class StudentsController : AbstractController<Student>
     {
         public StudentsController()
         {
             Repository = new StudentRepository();
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] StudentDTO entity)
+        {
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //TODO solve conflict at inserting group (not allowing null)
+            var newId = new Guid();
+            var newStudent = new Student
+            {
+                Id = newId,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                BirthDate = entity.BirthDate,
+                Courses = entity.Courses == null ? new List<Course>() : entity.Courses,
+                Group = entity.Group == null ? new Group() : entity.Group,
+                RegistrationNumber = entity.RegistrationNumber,
+                Year = entity.Year
+            };
+
+            Repository.Create(newStudent);
+
+            return CreatedAtRoute("GetResourceStudents", new { id = newId }, entity);
+        }
+
     }
 }
+
+
+/*
+ [HttpPost]
+        public IActionResult Post([FromBody] StudentDTO entity)
+        {
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Guid newId = new Guid();
+            var newStudent = new Student
+            {
+                Id = newId,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                BirthDate = DateTime.Now,
+                Courses = new List<Course>(),
+                Group = "4A5",
+                RegistrationNumber = "hfha813187",
+                Year = 2
+            };
+
+            Repository.Create(newStudent);
+
+            return CreatedAtRoute("GetResourceStudents", new { id = newId }, entity);
+        }
+     */
